a8db069f67b4a81306ef2609963a83c277847e0b
markdicks <markdicks03@gmail.com>
2025-05-23 15:12:27 +0200

    Add project files.

---
+++ wwwroot/js/map.js
@@ -0,0 +1,21 @@
+﻿var map = L.map('map').setView([0, 0], 2);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+var marker;
+
+function showLocation() {
+    var lat = parseFloat(document.getElementById('latitude').value);
+    var lon = parseFloat(document.getElementById('longitude').value);
+
+    if (!isNaN(lat) && !isNaN(lon)) {
+        if (marker) {
+            map.removeLayer(marker);
+        }
+        marker = L.marker([lat, lon]).addTo(map);
+        map.setView([lat, lon], 13);
+    } else {
+        alert("Please enter valid coordinates.");
+    }
+}
